253afa02b87dc0b2fe51b97fba2a3addf6e14efe
Olavo e Souto <olavo_souto@hotmail.com>
2022-05-09 20:06:52 -0300

    deploy

--- src/database/db.js
+++ src/database/db.js
@@ -1,13 +1,16 @@
 const Sequelize = require("sequelize");
 
 const connection = new Sequelize(
-  process.env.DB_BASE,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DATABASE_URL,
+  
   {
-    host: process.env.DB_HOST,
-    port: 5432,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnautorized: false,
+      },
+    },
   }
 );
 
